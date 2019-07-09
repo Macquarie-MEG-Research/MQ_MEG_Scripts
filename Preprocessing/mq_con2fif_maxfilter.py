41e4d6b43db2d0914ea0a5014944267b6cfee594
Robert Seymour <robbyseymour@gmail.com>
2019-07-09 14:47:16 +1000

    Convert KIT data to FIF & perform tSSS
    #9 First try of python script to convert KIT data to FIF & perform
    tSSS. Currently works, but Maxfilter options are hardcoded. Could also
    let the user specify SSS/tSSS  and/or the correlation value.
    
    There is also a bug in Fieldtrip not loading the gradiometer
    information from the saved .fif file. Some detective work needed!

---
+++ Preprocessing/mq_con2fif_maxfilter.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Convert KIT data to FIF & perform tSSS
+"""
+
+import argparse
+parser = argparse.ArgumentParser(description='.con to .fif + Maxwell Filter')
+required = parser.add_argument_group('required arguments')
+required.add_argument('-con',help="path to .con file", action="store",
+dest = "confile",required=True)
+required.add_argument('-elp',help="path to .elp file", action="store",
+dest = "elpfile",required=True)
+required.add_argument('-mrk',help="path to .mrk file", action="store",
+dest = "mrkfile",required=True)
+required.add_argument('-hsp',help="path to .hsp file", action="store",
+dest = "hspfile",required=True)
+
+args = parser.parse_args()
+
+confile = args.confile
+elpfile = args.elpfile
+mrkfile = args.mrkfile
+hspfile = args.hspfile
+
+# Extract the name of the confile and rename with _raw_tsss.fif
+result = confile.split('/')[-1]
+
+print('Converting %s to .fif file and applying maxwell filtering'%(result))
+
+result = result.replace('.con','')
+result = result.replace('.','_')
+result = result.replace(' ','_')
+result = result + '_raw_tsss.fif'
+
+print('Importing MNE')
+import mne
+from mne.preprocessing import maxwell_filter
+
+print('Loading data...')
+# Load Raw
+raw = mne.io.read_raw_kit(confile, mrk=mrkfile, elp=elpfile,
+hsp=hspfile,verbose=True)
+
+print('Perorming maxwell_filter (tSSS)')
+## Perform tSSS
+raw_tsss = maxwell_filter(raw,st_duration=60, st_correlation=0.9)
+
+print('Saving data')
+## Save as .fif file
+raw_tsss.save(result,buffer_size_sec=1)
