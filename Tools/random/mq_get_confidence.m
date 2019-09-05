6e96ca1916f514f21c58c391f67b8a8fb635eab7
Robert Seymour <robbyseymour@gmail.com>
2019-09-05 17:55:00 +1000

    Random change in mq_get_confidence

--- Tools/random/mq_get_confidence.m
+++ Tools/random/mq_get_confidence.m
@@ -11,7 +11,8 @@ function [yMean, yCI95] = mq_get_confidence(y)
 
 N = size(y,1);                                      
 yMean = mean(y);                                    % Mean Of All Experiments At Each Value Of ?x?
-ySEM = std(y)/sqrt(N);                              % Compute ?Standard Error Of The Mean? Of All Experiments At Each Value Of ?x?
+
+ySEM = std(y)/sqrt(N);                 % Compute ?Standard Error Of The Mean? Of All Experiments At Each Value Of ?x?
 CI95 = tinv([0.025 0.975], N-1);                    % Calculate 95% Probability Intervals Of t-Distribution
 yCI95 = bsxfun(@times, ySEM, CI95(:));              % Calculate 95% Confidence Intervals Of All Experiments At Each Value Of ?x?
 end
