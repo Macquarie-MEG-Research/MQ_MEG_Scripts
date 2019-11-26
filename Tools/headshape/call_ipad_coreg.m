8e31a4984a0a25b4f91b3bdf71c2f3f7cd9ed54c
Robert Seymour <robbyseymour@gmail.com>
2019-11-26 15:27:35 +1100

    Small changes

--- Tools/headshape/call_ipad_coreg.m
+++ Tools/headshape/call_ipad_coreg.m
@@ -1,7 +1,33 @@
-cfg = [];
-cfg.subject_number = '1111';
-cfg.subject_intial = 'RS';
-cfg.project_number = '176';
-cfg.scaling = 0.98;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 1. Add Fieldtrip and MQ_MEG_Scripts to your MATLAB path
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+path_to_MQ_MEG_Scripts = '';
+path_to_FT_toolbox     = '';
+
+disp('Adding Fieldtrip to your MATLAB path');
+addpath(path_to_FT_toolbox);
+ft_defaults
+
+disp('Adding MQ_MEG_Scripts to your MATLAB path');
+addpath(genpath(path_to_MQ_MEG_Scripts));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2. Change your Current Directory to where you want the .elp and .hsp file
+%    to be saved
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+cd('');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 3. Call mq_3D_coreg 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+cfg                 = [];
+cfg.subject_number  = '1111';
+cfg.subject_intials  = 'RS';
+cfg.project_number  = '176';
+cfg.scaling         = 0.98;
 mq_3D_coreg(cfg)
 
